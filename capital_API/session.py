7952b2cde2e7997efd1efaf7684a31062aca8655
yanpin0524 <a131234568@gmail.com>
2023-03-29 15:34:40 +0800

    FEAT: add  create_new_session feature

---
+++ capital_API/session.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+basic_url = 'https://demo-api-capital.backend-capital.com/api/v1'
+
+def create_new_session(identifier, password, API_KEY):
+    payload = json.dumps({
+        "identifier": identifier,
+        "password": password
+    })
+
+    headers = {
+        'X-CAP-API-KEY': API_KEY,
+        'Content-Type': 'application/json'
+    }
+
+    return requests.post(url='{}/session'.format(basic_url), data=payload, headers=headers)
--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-import test.get_prices as get_prices
+import capital_API.session as API_session
+import os
+from dotenv import load_dotenv
+from pprint import PrettyPrinter
 
-get_prices.writeMessage(__name__)
+load_dotenv()
+pp = PrettyPrinter(indent=2)
+
+profile = {
+    "identifier": os.getenv('identifier'),
+    "password": os.getenv('password'),
+    "API_KEY": os.getenv('X-CAP-API-KEY')
+}
+
+res = API_session.create_new_session(
+    profile['identifier'], profile['password'], profile['API_KEY'])
+
+pp.pprint(res.json())
